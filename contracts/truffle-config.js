429502f1083f2c32cdb088abdaebeb2e34f0add7
Mircea Gosman <mirceagosman@gmail.com>
2022-01-29 13:33:35 -0500

    Ropsten Network Truffle config

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -26,7 +26,7 @@
 
 var HDWalletProvider = require("@truffle/hdwallet-provider");
 var mnemonic =
-  "village lady curious birth protect grid apple merry awake second bullet level";
+    "age census owner lunar salon egg hill truck chuckle ocean hammer egg";
 
 module.exports = {
   /**
@@ -72,14 +72,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://eth-ropsten.alchemyapi.io/v2/7SXH-5Awi-PXwykyzs82zwSjysQePayV`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
